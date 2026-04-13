d3aa40a4aaccea89818f62b3e1e4cfc7965b4858
Alex Hurrell <alex@clearmycredit.co.uk>
2026-04-13 16:46:01 +0100

    Add CORS middleware to application configuration for improved preflight request handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(HandleCors::class);
+
         $middleware->validateCsrfTokens(except: [
             'leads/*/temp-mail/generate',
             'leads/*/temp-mail/reset',
