eb3d5bd4452fe5d63f9a2c9e387f682ee5981269
Alex Hurrell <alex@clearmycredit.co.uk>
2026-04-19 10:50:22 +0100

    Update node binary path in prepareRun method of CreditCheckV2Controller for improved flexibility in script execution

--- app/Http/Controllers/CreditCheckV2Controller.php
+++ app/Http/Controllers/CreditCheckV2Controller.php
@@ -196,8 +196,10 @@ class CreditCheckV2Controller extends Controller
         $script = base_path('scripts/credit-check-v2.mjs');
         $timeout = (float) env('CREDIT_CHECK_V2_PROCESS_TIMEOUT', 7200);
 
+        $nodeBinary = (string) env('NODE_BINARY', '/root/.nvm/versions/node/v24.11.0/bin/node');
+
         $process = new Process(
-            ['node', $script, $payloadPath],
+            [$nodeBinary, $script, $payloadPath],
             base_path(),
             null,
             null,
