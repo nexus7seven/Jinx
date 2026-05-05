fd5926f21d4ba6d82252ab6ff3e1bcee80e38ac5
nexus7seven <106931323+nexus7seven@users.noreply.github.com>
2026-05-05 16:35:44 +0100

    Fix scheduler gating to use config flags

---
+++ config/remarketing.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'cbna_scanner_enabled' => env('REMARKETING_CBNA_SCANNER_ENABLED', false),
+    'linear_executor_enabled' => env('REMARKETING_LINEAR_EXECUTOR_ENABLED', false),
+];
