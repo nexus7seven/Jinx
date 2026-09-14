eb53daaf0370dbb8c9f27e0c2014499467dbad76
nexus7seven <106931323+nexus7seven@users.noreply.github.com>
2026-09-14 20:42:43 +0100

    Fix assistant message relationship foreign key

--- app/Models/AssistantConversation.php
+++ app/Models/AssistantConversation.php
@@ -25,6 +25,6 @@ class AssistantConversation extends Model
 
     public function messages()
     {
-        return $this->hasMany(AssistantMessage::class);
+        return $this->hasMany(AssistantMessage::class, 'conversation_id');
     }
 }
